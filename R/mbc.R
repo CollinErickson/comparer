f398be53c43e6d6d557738516ce1a5a01e8f1b8c
Collin Erickson <collinberickson@gmail.com>
2017-07-07 21:49:46 -0400

    Getting better output

--- R/mbc.R
+++ R/mbc.R
@@ -7,19 +7,20 @@
 #' @export
 #'
 #' @examples
-#' m1 <- mbc()
-mbc <- function(..., times=1) {
+#' m1 <- mbc(function(x) {Sys.sleep(rexp(1, 30));mean(x)}, function(x) {Sys.sleep(rexp(1, 5));median(x)}, input=runif(100))
+mbc <- function(..., times=5, input) {#browser()
   dots <- list(...)
   n <- length(dots)
   runtimes <- matrix(NA, n, times)
-  outs <- list()
+  outs <- rep(list(rep(list(NA),times)), n)
   for (i in 1:n) {
     for (j in 1:times) {
       runtime <- system.time(
-        out <- dots[[i]]()
+        out <- dots[[i]](input)
       )
-      runtimes[i, j] <- runtime$elapsed
+      runtimes[i, j] <- runtime['elapsed']
       outs[[i]][[j]] <- out
     }
   }
+  plyr::adply(runtimes, 1, function(x) data.frame(min=min(x), med=median(x), mean=mean(x), max=max(x)), .id = 'Func')
 }
