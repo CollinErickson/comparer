6aece27435968fa161c263dce6b0ed4277665b8f
Collin Erickson <collinberickson@gmail.com>
2017-07-31 21:55:31 -0400

    Working on mbc, added post

--- R/comparer.R
+++ R/comparer.R
@@ -1,7 +1,9 @@
 comparer <- R6::R6Class(
   classname = "comparer",
   public = list(
+    initialize = function() {
 
+    }
   ),
   private = list(
 
--- R/mbc.R
+++ R/mbc.R
@@ -8,11 +8,13 @@
 #'
 #' @examples
 #' m1 <- mbc(function(x) {Sys.sleep(rexp(1, 30));mean(x)}, function(x) {Sys.sleep(rexp(1, 5));median(x)}, input=runif(100))
-mbc <- function(..., times=5, input) {#browser()
+#' mbc(function(x) {Sys.sleep(rexp(1, 30));mean(x)}, function(x) {Sys.sleep(rexp(1, 5));median(x)}, input=runif(100), post=function(x){c(x+1, 12)})
+mbc <- function(..., times=5, input, post, target) {#browser()
   dots <- list(...)
   n <- length(dots)
   runtimes <- matrix(NA, n, times)
   outs <- rep(list(rep(list(NA),times)), n)
+  # if (!missing(post)) {postout <- array(data = NA, dim = c(n, times, post_length))} #rep(list(rep(list(NA),times)), n)}
   for (i in 1:n) {
     for (j in 1:times) {
       runtime <- system.time(
@@ -20,7 +22,20 @@ mbc <- function(..., times=5, input) {#browser()
       )
       runtimes[i, j] <- runtime['elapsed']
       outs[[i]][[j]] <- out
+      if (!missing(post)) {
+        po <- post(out)
+        if (i==1 && j==1) { # Initialize once we know length
+          postout <- array(data = NA, dim = c(n, times, length(po)))
+        }
+        postout[i,j,] <- po
+      }
     }
   }
-  plyr::adply(runtimes, 1, function(x) data.frame(min=min(x), med=median(x), mean=mean(x), max=max(x)), .id = 'Func')
+  browser()
+  times_df <- plyr::adply(runtimes, 1, function(x) data.frame(min=min(x), med=median(x), mean=mean(x), max=max(x)), .id = 'Func')
+  if (!missing(post)) {
+    post_df <- plyr::adply(postout, c(1,3), function(x) data.frame(min=min(x), med=median(x), mean=mean(x), max=max(x)), .id = c('Func','Stat'))
+    return(list(times_df, post_df))
+  }
+  times_df
 }
