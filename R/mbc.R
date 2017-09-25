1f69d7525ebbeee9ee3aff6dc1bace341a8c7780
Collin Erickson <collinberickson@gmail.com>
2017-09-25 17:02:21 -0500

    Started auto-post-processing

--- R/mbc.R
+++ R/mbc.R
@@ -22,24 +22,46 @@
 #'   post=function(x){c(x+1, 12)}, times=3)
 #' m1 <- mbc(function() {x <- runif(100);Sys.sleep(rexp(1, 30));mean(x)},
 #'   function() {x <- runif(100);Sys.sleep(rexp(1, 5));median(x)})
-mbc <- function(..., times=5, input, inputi, post, target) {#browser()
+mbc <- function(..., times=5, input=NULL, inputi=NULL, post, target) {#browser()
+  if (!is.null(input) && !is.null(inputi)) {
+    stop("input and inputi should not both be given in")
+  }
+  # dots are the functiosn to run, n is the number of functions
   dots <- list(...)
   n <- length(dots)
+
+  fnames <- names(dots)
+  fnoname <- which(fnames == "")
+  fnames[fnoname] <- paste0("f", fnoname)
+
+  # Create objects to hold output data
   runtimes <- matrix(NA, n, times)
+  dimnames(runtimes)[[1]] <- fnames
   outs <- rep(list(rep(list(NA),times)), n)
   # if (!missing(post)) {postout <- array(data = NA, dim = c(n, times, post_length))} #rep(list(rep(list(NA),times)), n)}
-  for (i in 1:n) {
-    for (j in 1:times) {
+
+  # Loop over each replicate
+  for (j in 1:times) {
+    # Get input for replicate if inputi given
+    if (!is.null(inputi)) {
+      if (is.function(inputi)) {
+        input <- inputi(j)
+      } else {
+        input <- inputi[[j]]
+      }
+    }
+    # Loop over each function
+    for (i in 1:n) {
       # See if there is input to each
       if (!missing(input)) { # Single input for all
         runtime <- system.time(
           out <- dots[[i]](input)
         )
-      } else if (!missing(inputi)) { # Different input for each rep
-        runtime <- system.time(
-          out <- dots[[i]](inputi(j))
-        )
-      } else {
+      # } else if (!missing(inputi)) { # Different input for each rep
+      #   runtime <- system.time(
+      #     out <- dots[[i]](inputi(j))
+      #   )
+      } else { # No input at all
         runtime <- system.time(
           out <- dots[[i]]()
         )
@@ -56,13 +78,15 @@ mbc <- function(..., times=5, input, inputi, post, target) {#browser()
     }
   }
   # browser()
+
+  # Create list to retun, set to class mbc so S3 methods can be used
   out_list <- list()
   class(out_list) <- c("mbc", class(out_list))
   out_list$Run_times <-
     if (times > 5) {
-      plyr::adply(runtimes, 1, function(x) data.frame(min=min(x), med=median(x), mean=mean(x), max=max(x)), .id = 'Func')
+      plyr::adply(runtimes, 1, function(x) data.frame(min=min(x), med=median(x), mean=mean(x), max=max(x)), .id = 'Function')
     } else {
-      plyr::adply(runtimes, 1, function(x) {sx <- sort(x); c((sx), mean=mean(x))}, .id = 'Func')
+      plyr::adply(runtimes, 1, function(x) {sx <- sort(x); c((sx), mean=mean(x))}, .id = 'Function')
     }
   if (!missing(post)) {
     if (times > 5) {
@@ -70,11 +94,39 @@ mbc <- function(..., times=5, input, inputi, post, target) {#browser()
     } else {
       post_df_disp <- plyr::adply(postout, c(1,3), function(x) {sx <- sort(x); c((sx), mean=mean(x))}, .id = c('Func','Stat'))
     }
-    # t1 <- list('Run times'=times_df, 'Output'=postout, 'Output_disp'=post_df_disp)
-    # class(t1) <- c("mbc", class(t1))
-    # return(t1)
+    out_list$RawOutput <- outs
     out_list$Output <- postout
     out_list$Output_disp <- post_df_disp
+  } else {
+    # Check if all outs have same length, then convert to df if small enough just as postprocessing would
+    #  in case single return value already is post
+    lengths <- sapply(outs, function(listi) {sapply(listi, length)})
+    len <- lengths[[1]]
+    if (all(lengths == len)) { # Try to auto-post-process
+      # Convert data to array
+      postout <- array(data = NA, dim = c(n, times, len))
+      for (i in 1:n) {
+        for (j in 1:times) {
+          postout[i, j, ] <- outs[[i]][[j]]
+        }
+      }
+      # Post-process
+      if (times > 5) {
+        post_df_disp <- plyr::adply(postout, c(1,3), function(x) data.frame(min=min(x), med=median(x), mean=mean(x), max=max(x)), .id = c('Func','Stat'))
+      } else {
+        post_df_disp <- plyr::adply(postout, c(1,3), function(x) {sx <- sort(x); c((sx), mean=mean(x))}, .id = c('Func','Stat'))
+      }
+      # Set outputs
+      out_list$RawOutput <- outs
+      out_list$Output <- postout
+      out_list$Output_disp <- post_df_disp
+    } else { # Can't auto-post-process
+      out_list$Output <- outs
+    }
+  }
+  # browser()
+  if (is.array(out_list$Output)) {
+    dimnames(out_list$Output)[[1]] <- fnames
   }
   out_list
 }
@@ -112,7 +164,13 @@ plot.mbc <- function(x, ...) {
 print.mbc <- function(x, ...) {#browser()
   nam <- names(x)
   if ('Run_times' %in% nam) {
+    cat("Run times (sec)\n")
     print(x$Run_times)
   }
-  print(x$Output)
+  if ('Output' %in% nam) {
+    if (is.data.frame(x$Output) || is.numeric(x$Output)) { # Only print df, not list
+      cat("\nOutput \n")
+      print(x$Output)
+    }
+  }
 }
