c70263bb0d675e2d18577d0197431ce43a7a1aae
Collin Erickson <collinberickson@gmail.com>
2019-01-15 22:49:09 -0600

    Trying to microbenchmark for timing

---
+++ scratch/scratch_trytocapturemicrobenchmarkoutput.R
@@ -0,0 +1,13 @@
+tf <- function(...) {
+
+  dots <- as.list(match.call(expand.dots = FALSE)$`...`)
+  print(dots)
+  o1 <- list()
+  f1 <- function(i) {o1[[i]] <<- eval(dots[[1]])}
+  browser()
+  f1(1)
+  mb1 <- list()
+  f2 <- function(i) {mb1[[i]] <<- microbenchmark::microbenchmark(o1[[i]] <<- eval(dots[[1]]))}
+  print(o1)
+}
+tf(2*a)
