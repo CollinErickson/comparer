20040c595a03371f7bb416162e1003c40febf5a6
Collin Erickson <collinberickson@gmail.com>
2019-08-21 07:57:43 -0400

    more hyperparam

--- scratch/scratch_hype.R
+++ scratch/scratch_hype.R
@@ -28,17 +28,39 @@ hype <- R6::R6Class(
   classname="hype",
   inherit=ffexp,
   public=list(
+    exp = NULL,
+    eval_func = NULL,
     initialize = function(eval_func, ...) { # ... is params
       self$eval_func <- eval_func
       dots <- list(...)
+      parlist <- data.frame()
+      parnames <- c()
+      parlow <- c()
+      parhigh <- c()
       for (pari in dots) {
         if (!("par_hype" %in% class(pari))) {
           stop("All ... should be par_hype objects")
         }
+        parlist <- c(parlist, pari)
+        parnames <- c(parnames, pari$name)
+        parlows <- c(parlows, pari$low)
+        parhighs <- c(parhighs, pari$high)
       }
 
     },
     add_data = function(X, Y) {
+
+    },
+    add_X = function(X) {
+
+    },
+    add_LHS = function(n) {
+
+    },
+    add_EI = function(n) {
+
+    },
+    run_all = function() {
 
     }
   )
