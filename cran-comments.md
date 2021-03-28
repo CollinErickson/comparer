bb07f4b0ed2ec2e2216f10a541bdcd799b1d83aa
Collin Erickson <collinberickson@gmail.com>
2021-03-28 08:54:26 -0400

    Preparing for release

--- cran-comments.md
+++ cran-comments.md
@@ -8,8 +8,26 @@ I made some minor updates to the existing functions in the package.
 
 ## R CMD check results
 
+On local Ubuntu and Ubuntu on Travis:
+
 0 errors | 0 warnings | 0 note
 
+From R-hub, there are 0 errors or warnings,
+only a few notes:
+
+"
+* checking CRAN incoming feasibility ... NOTE
+Maintainer: 'Collin Erickson <collinberickson@gmail.com>'
+Possibly mis-spelled words in DESCRIPTION:
+  ffexp (13:18)
+
+Uses the superseded package: 'snow'
+"
+
+The first isn't an issue.
+For 'snow', I tried removing it from Suggests since I never actually
+call it, I only use 'parallel', but then it gives an error.
+Thus I am leaving 'snow' in Suggests.
 
 ## Reverse dependencies
 
--- scratch/MultiArmedBandit.R
+++ scratch/MultiArmedBandit.R
@@ -18,18 +18,19 @@
       self$ninputs <- length(inputs)
       self$method <- method
       # print(inputs); print(n0)
-      self$inputs2 <- data.frame(input=inputs, K=n0)
+      stopifnot(is.numeric(n0), length(n0) == 1)
+      self$inputs2 <- expand.grid(input=inputs, K=1:n0)
       self$exp <- comparer::ffexp$new(input=self$inputs2, eval_func=eval_func)
       self$minimize <- minimize
     },
-    run = function(verbose=0) {
-      self$exp$run_all(verbose=verbose)
+    run = function(verbose=0, ...) {
+      self$exp$run_all(verbose=verbose, ...)
     },
-    add1 = function() {
+    add1 = function(run=TRUE) {
       minmult <- if (self$minimize) {-1} else {1}
       ocdf <- self$exp$outcleandf
       countdf <- ocdf %>% group_by(input) %>%
-        summarize(N=n(),mn=mean(V1), std=sd(V1),
+        summarize(N=n(),mn=mean(V1, na.rm=T), std=sd(V1, na.rm=T),
                   UCB=mn+2*std, LCB=mn-2*std)
       if (min(countdf$N) < 2) {
         nextind <- countdf %>% arrange(N) %>% .$input %>% .[[1]]
@@ -51,7 +52,7 @@
         }
         nextind <- nextdf$input[1]
         nextK <- nextdf$N[1] + 1
-      } else if (self$method=="TS") {
+      } else if (self$method=="TS") {browser()
         countdf <- countdf %>% mutate(samp=mn+std*rt(n=n(), df=N-1))
         if (self$minimize) {
           nextdf <- countdf %>% arrange(samp)
@@ -68,12 +69,24 @@
         stop(paste0("Bad method given (", self$method,"), pick one of epsgreedy, CB, TS, or fewest"))
       }
       # cat('next ind is', nextind, 'nextK', nextK, '\n')
+      if (is.na(nextind)) {browser()}
       # Add new level to experiment, returns new object
       expnew <- self$exp$add_level("input", list(input=nextind, K=nextK), suppressMessage = T)
       self$exp <- expnew
       # Run the new one
       # self$exp$run_all(verbose=0)
-      self$run(verbose=0)
+      if (run) {
+        self$run(verbose=0)
+      }
+    },
+    addn = function(n, run=TRUE, parallel) {
+      stopifnot(is.integer(n), length(n)==1)
+      for (i in 1:n) {
+        self$add1(run=FALSE)
+      }
+      if (run) {
+        self$run(verbose=0)
+      }
     },
     plot = function(flip=F) {
       df <- self$exp$outcleandf
@@ -104,17 +117,35 @@
   )
 )
 
-e1 <- ..MAB_R6$new(1:5, n0=1, eval_func=function(input, ...) {rnorm(1, input, 2)}, minimize=F, method="TS")
-e1$exp
-e1$run() #exp$run_all()
-e1$exp$outcleandf
-library(dplyr); library(ggplot2)
-e1$add1()
-e1$exp
-for (i in 1:10) {
+if (F) {
+  e1 <- ..MAB_R6$new(1:5, n0=1, eval_func=function(input, ...) {rnorm(1, input, 2)}, minimize=F, method="TS")
+  e1$exp
+  e1$run() #exp$run_all()
+  e1$exp$outcleandf
+  library(dplyr); library(ggplot2)
   e1$add1()
-  # print(e1$exp$outcleandf)
-  # plot(e1$exp$outcleandf$input, e1$exp$outcleandf$V1)
-  e1$plot() %>% print
+  e1$exp
+  for (i in 1:10) {
+    e1$add1()
+    # print(e1$exp$outcleandf)
+    # plot(e1$exp$outcleandf$input, e1$exp$outcleandf$V1)
+    e1$plot() %>% print
+  }
+  print(e1)
 }
-print(e1)
+
+
+if (F) {
+
+  e1 <- ..MAB_R6$new(1:5, n0=2, eval_func=function(input, ...) {print(input);rnorm(1, input, 2)}, minimize=F, method="TS")
+  e1$exp$rungrid2()
+  e1$run()
+  e1$plot()
+  e1$add1(run=F)
+  e1$add1(run=F)
+  e1$add1(run=F)
+  e1
+  e1$run()
+
+}
+
